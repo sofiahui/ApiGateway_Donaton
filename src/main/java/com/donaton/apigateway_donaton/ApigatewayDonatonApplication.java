ed0d7e8225cdeece9cb61b1ebdd08ca5f7125625
sofiahui <so.huichulef@duocuc.cl>
2026-04-11 17:08:34 -0400

    primer commit

---
+++ src/main/java/com/donaton/apigateway_donaton/ApigatewayDonatonApplication.java
@@ -0,0 +1,13 @@
+package com.donaton.apigateway_donaton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayDonatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayDonatonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donaton/apigateway_donaton/ApigatewayDonatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donaton.apigateway_donaton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayDonatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
