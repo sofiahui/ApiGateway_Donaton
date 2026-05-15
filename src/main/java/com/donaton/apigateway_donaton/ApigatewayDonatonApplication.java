7766185a8142a2fa3828e2794915fb1dbf71a79b
sofiahui <so.huichulef@duocuc.cl>
2026-05-15 15:17:45 -0400

    feat cors

--- src/main/java/com/donaton/apigateway_donaton/ApigatewayDonatonApplication.java
+++ src/main/java/com/donaton/apigateway_donaton/ApigatewayDonatonApplication.java
@@ -2,6 +2,10 @@ package com.donaton.apigateway_donaton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class ApigatewayDonatonApplication {
@@ -10,4 +14,15 @@ public class ApigatewayDonatonApplication {
 		SpringApplication.run(ApigatewayDonatonApplication.class, args);
 	}
 
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("http://localhost:5173");
+		config.addAllowedMethod("*");
+		config.addAllowedHeader("*");
+		config.setAllowCredentials(true);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return new CorsWebFilter(source);
+	}
 }
